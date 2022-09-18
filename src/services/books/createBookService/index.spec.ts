1a330c2dcd3c643d32e69cfc3ae9e303ecbe337d
eduardamirelly <mirelly.eduarda404@gmail.com>
2022-09-18 03:51:39 -0300

    :white_check_mark: chore: update tests createBookService

--- src/services/books/createBookService/index.spec.ts
+++ src/services/books/createBookService/index.spec.ts
@@ -4,11 +4,11 @@ import app from "../../../server";
 
 test("it should be possible to create a book...", async () => {
   const response = await request(app).post("/books").send({
-    title: 'A hipótese do amor 2',
-    description: 'Alguma coisa 2',
-    author: 'Alguém 2',
+    title: 'Name example',
+    description: 'Description example',
+    author: 'Author example',
     price: 25.00,
-    cover: 'cover',
+    cover: 'Cover example',
     isFavorite: false,
   })
 
@@ -17,10 +17,10 @@ test("it should be possible to create a book...", async () => {
 
 test("it shouldn't be possible to create a book if a missing required fields, like author for example...", async () => {
   const response = await request(app).post("/books").send({
-    title: 'A hipótese do amor 2',
-    description: 'Alguma coisa 2',
+    title: 'Name example',
+    description: 'Description example',
     price: 25.00,
-    cover: 'cover',
+    cover: 'Cover example',
     isFavorite: false,
   })
 
