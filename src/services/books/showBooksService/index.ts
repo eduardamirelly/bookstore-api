7709bc5109c5a665db4e9edcad045352b96f1e85
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-01-22 14:19:08 -0300

    :sparkles: add filters to showBooksService

--- src/services/books/showBooksService/index.ts
+++ src/services/books/showBooksService/index.ts
@@ -2,29 +2,81 @@ import { prisma } from "../../../prisma/client";
 
 import { BookType } from "../../../@types/book";
 import { Request } from "express";
+import { ParsedUrlQuery } from "querystring";
 
 type ShowBooksResponse = BookType[];
+
+interface RequestProps extends ParsedUrlQuery {
+  search?: string;
+  sort?: 'asc' | 'desc';
+  category?: string;
+}
 
 export class ShowBooksService {
   async execute(request: Request): Promise<ShowBooksResponse> {
 
-    const { query } = request;
+    const query = request.query as RequestProps;
 
+    const search = query.search || '';
+    const sort = query.sort || 'desc';
+    const category = query.category || '';
 
-    console.log(query);
+    let books = [];
 
-    // Get category by request.query
-    // Know if have relation with some book
-
-    const books = await prisma.book.findMany({
-      include: {
-        categories: {
-          select: {
-            category: true
+    if(category) {
+      books = await prisma.book.findMany({
+        orderBy: [
+          {
+            createdAt: sort,
+          },
+          {
+            title: sort,
+          }
+        ],
+        where: {
+          categories: {
+            some: {
+              category: {
+                name: category,
+              }
+            },
+          },
+          title: {
+            contains: search,
+          }
+        },
+        include: {
+          categories: {
+            select: {
+              category: true
+            }
           }
         }
-      }
-    });
+      });
+    } else {
+      books = await prisma.book.findMany({
+        orderBy: [
+          {
+            createdAt: sort,
+          },
+          {
+            title: sort,
+          }
+        ],
+        where: {
+          title: {
+            contains: search,
+          }
+        },
+        include: {
+          categories: {
+            select: {
+              category: true
+            }
+          }
+        }
+      });
+    }
 
     return books;
   }
