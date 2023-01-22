0f64e13fd1479fbca077666311a1c0df47c7d586
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-01-21 22:07:08 -0300

    :white_check_mark: add test to create book with categories

--- src/services/books/createBookService/index.spec.ts
+++ src/services/books/createBookService/index.spec.ts
@@ -14,6 +14,25 @@ test("it should be possible to create a book...", async () => {
   expect(response.statusCode).toBe(201);
 })
 
+test("it should be possible to create a book with categories...", async () => {
+  const responseCategoryCreated = await request(app).post("/categories").send({
+    name: "Romance",
+  });
+
+  expect(responseCategoryCreated.statusCode).toBe(201);
+
+  const responseBookCreated = await request(app).post("/books").send({
+    title: 'Title example',
+    description: 'Description example',
+    author: 'Author example',
+    price: 25.00,
+    cover: 'Cover example',
+    categories: ["Romance"]
+  });
+
+  expect(responseBookCreated.statusCode).toBe(201);
+})
+
 test("it shouldn't be possible to create a book if a missing required fields, like author for example...", async () => {
   const response = await request(app).post("/books").send({
     title: 'Title example',
